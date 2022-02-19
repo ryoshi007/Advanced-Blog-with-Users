56da0be1ade337f53d8d3f9a166de37e841fc7bf
ryoshi007 <yanjian1510@gmail.com>
2022-02-20 00:30:55 +0800

    Uncomment code for creating database for Heroku

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ class Comment(db.Model):
     author = relationship("User", back_populates="comments")
     parent_post = relationship("BlogPost", back_populates="comments")
 
-db.create_all()
+# db.create_all()
 
 
 @app.route('/')
